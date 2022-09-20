b1dfa122b0d4c22f9961039b56f587692f7c4262
jdelgado1979 <johandelgado637@gmail.com>
2022-09-21 00:57:50 +0200

    repeat string
    add function that repeats strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num<0){
+        return 'ERROR';
+        
+       } else {
+        let x = [];
+        for(var i =0; i<num; ++i){
+        x.push(string);
+        }
+        return  x.join('')
+       }
 };
 
 // Do not edit below this line
