71ed27dae5630570ffe6a74cb5916e6f0a34bd23
jdelgado1979 <johandelgado637@gmail.com>
2022-09-21 13:37:04 +0200

    leap years
    add function that determines leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
+const leapYears = function(a) {
+    if (( a % 4 === 0) && ( a % 100 !== 0)) {
+       return true;
+     } else if (( a % 100 === 0) && ( a % 400 === 0)){
+       return true;
+     } else return false  
+   }
 
 // Do not edit below this line
 module.exports = leapYears;
