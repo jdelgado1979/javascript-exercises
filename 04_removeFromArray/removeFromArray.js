2ec96226fa40efce5042b53f75c54b070b3681fe
jdelgado1979 <johandelgado637@gmail.com>
2022-09-21 03:23:00 +0200

    remove from array
    add function that removes elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,33 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function([...args], ...z) {
+    let a = [...args];
+    let b = [...z];
+   
+   
+   let c = [];
+    for(let i = 0; i<a.length;i++){
+      for(let j = 0; j<b.length;j++){
+       if(a[i]===b[j]){
+         c.push(a[i]);
+        } else {
+          continue;
+       }
+      }
+    }
+    
+   const setA = new Set([...args]); 
+   const setC = new Set(c);  
+   const _difference = new Set(setA);
+    for (const elem of setC) {
+      if (_difference.has(elem)) {
+        _difference.delete(elem);
+      } else {
+        _difference.add(elem);
+      }
+    }
+    let arr = Array.from(_difference);
+    return arr;
+  };
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
