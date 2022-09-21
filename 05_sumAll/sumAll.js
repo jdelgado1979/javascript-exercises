ec94caa5a896293bd6fa4f1f46988dc199dbabc7
jdelgado1979 <johandelgado637@gmail.com>
2022-09-21 13:13:47 +0200

    Sum All function
    Implement a function that adds everything in between 2 numbers, return error if input is not a positive integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a,b) {
+    if ((typeof a === 'string') || (typeof b === 'string')) {
+       return 'ERROR';
+     } 
+     
+     if ((a >= 0) && (b >=0) ) {
+         let c = [];
+         if (a<b) {
+            for(i=a; i <= b;i++) {
+                     c.push(i);
+                                 }
+             return c.reduce((a, b) => a + b);
+                   
+          } else if (a>b) {
+                     for(i=b; i <= a;i++){
+                              c.push(i);
+                                        }
+             return c.reduce((a, b) => a + b);
+         } 
+     
+      } else return 'ERROR'
+   }
+   
+   
 // Do not edit below this line
 module.exports = sumAll;
