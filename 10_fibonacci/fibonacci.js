387746c5508c5f2eab3e94599b8862a0efbd4369
jdelgado1979 <johandelgado637@gmail.com>
2022-10-08 23:39:36 +0200

    Fibonacci
    add function that identifies fibonacci numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) return 'OOPS';
+  do {
+    let fibonArray = [1,1]
+ for(let i=0;i<num;i++){
+  let a = fibonArray[i]+fibonArray[i+1];
+  fibonArray.push(a)
+}
+  return fibonArray[num-1];
+  } while(num>1);
 };
 
 // Do not edit below this line
