262b989d644ebda0e26b9e4f8f05736a66457452
jdelgado1979 <johandelgado637@gmail.com>
2022-10-08 22:56:28 +0200

    calculator
    Add math functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
+};
+	const sum = function(array) {
+
+    if (array.length == 0){
+      return 0;
+     } else {
+      let ans = array.reduce(function(a,b){return a+b});
+       return ans;
+       }
+     
+   };
+
+const multiply = function(array) {
+  if (array.length == 0){
+    return 0;
+   } else {
+    let ans = array.reduce(function(a,b){return a*b});
+     return ans;
+     }
 };
 
-const sum = function() {
-	
+const power = function(a,b) {
+  
+  let c = [];
+  for(let i=1; i<=b;++i){
+      c.push(a);
+    }
+    let d = c.reduce(function(a,b){return a*b});
+    return d;
+
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  if ((a==0)||(a==1)) {
+    return 1;
+  } else {
+    let c = [];
+    for(let i=1; i<=a;++i){
+    c.push(i);
+     }
+    let d = c.reduce(function(a,b){return a*b});
+     return d;
+    }
 };
 
 // Do not edit below this line
