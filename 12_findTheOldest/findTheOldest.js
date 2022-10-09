2e8b357b771bcaf6b495fa1664cb994af056f020
jdelgado1979 <johandelgado637@gmail.com>
2022-10-09 23:34:19 +0200

    findTheOldest
    add function to find the oldest in an array of objects using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(objectArray) {
+  
+    return objectArray.reduce((previousValue, currentValue) => {
+     
+    const age1 = (currentValue.yearOfDeath ?? 2022) - currentValue.yearOfBirth;
+    const age2 = (previousValue.yearOfDeath ?? 2022) - previousValue.yearOfBirth;
+    
+   if (age1  > age2) {
+    return currentValue
+   }
+   return previousValue;
+ })
+ };
 
 // Do not edit below this line
 module.exports = findTheOldest;
